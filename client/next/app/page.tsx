cd9d7b7d024104922d2acea46641b6d96da4998f
Aadarsha-prog <thapamagaraadarsha97@gmail.com>
2026-09-08 07:13:42 +0545

    feat: improve UI with Tailwind and shadcn components

--- client/next/app/page.tsx
+++ client/next/app/page.tsx
@@ -2,5 +2,17 @@ import { getTodos } from "./api";
 
 export default async function Home() {
   const todos = await getTodos();
-  return <main>{JSON.stringify(todos)}</main>;
+  return (
+    <main className="mx-auto max-w-2xl p-6">
+      <h1 className="mb-4 text-3xl text-gray-800 font-bold">
+        Todos
+      </h1>
+        <ul>
+          {todos.map(todo => (
+            <li className="mb-2" key={todo.id}>{todo.id}: {todo.title}</li>
+          ) )}
+        </ul>
+      
+    </main>
+  );  
 }
--- client/next/app/post/page.tsx
+++ client/next/app/post/page.tsx
@@ -1,15 +1,35 @@
+import{
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+
+
 import Link from "next/link";
 import { POSTS } from "./data";
+import {revalidatePath} from "next/cache";  
 
 function Posts() {
   async function deletePost(postId: string) {
     "use server";
+
+    const postIndex = POSTS.findIndex((post) => post.id === Number(postId));
+
+    if(postIndex === -1) {
+      throw new Error("Post not found");
+    }
+
+    POSTS.splice(postIndex, 1);
+    revalidatePath("/post");
   }
   return (
     <>
       {POSTS.length <= 0 ? (
         <EmptyContent />
       ) : (
+        
         <Link className="text-blue-500 underline" href="/post/create">
           Create New Post
         </Link>
@@ -20,17 +40,25 @@ function Posts() {
           key={post.id}
           className="flex items-center justify-between gap-2 bg-foreground/95 p-4 rounded-lg hover:bg-foreground/90"
         >
-          <div className="flex flex-col gap-1 w-max hover:underline text-background">
-            <Link href={`/post/${post.id}`} className="cursor-pointer">
-              <h2 className="text-lg text-background">{post.title}</h2>
-              <p className="text-xs text-background line-clamp-2 text-ellipsis">
+          <Card key={post.id}>
+            <CardHeader>
+              <CardTitle>
+                <Link href={`/post/${post.id}`}>{post.title}</Link>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-sm text-muted-foreground">
                 {post.description}
               </p>
-            </Link>
-          </div>
-          <button className="p-2 rounded-lg cursor-pointer hover:bg-red-100 text-xs bg-red-200 border border-red-500 text-red-500 ">
-            🗑️ Delete
-          </button>
+  
+      
+          <form action={deletePost.bind(null, post.id.toString())}>
+            <Button type="submit" variant="destructive" size="sm">
+              🗑️ Delete
+            </Button>
+          </form>
+          </CardContent>
+          </Card>
         </div>
       ))}
     </>
